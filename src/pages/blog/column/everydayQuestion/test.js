c0895e34db17308e5d8a12569d54a375868f8777
zyliang <zyliang@servyou.com.cn>
2021-04-12 18:56:29 +0800

    调整

--- src/pages/blog/column/everydayQuestion/test.js
+++ src/pages/blog/column/everydayQuestion/test.js
@@ -1,13 +1,5 @@
-import React from "react";
-
-export default function Child(props) {
-    const { value, valueChange } = props;
-    return (
-        <>
-            <div> 接收父组件的值: {value} </div>
-            <button onClick={() => valueChange("Changed Msg")}>
-                修改父组件的值
-            </button>
-        </>
-    );
+export function test(props){
+    return <div className='123'>
+        测试
+    </div>
 }
