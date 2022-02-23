438a1fe45513a9b74d2a42d87b4082d340bc1b51
zyliang <zyliang@servyou.com.cn>
2022-02-23 16:16:40 +0800

    新增

--- packages/algorithmStudy/src/helpers/sorting/Merge.ts
+++ packages/algorithmStudy/src/helpers/sorting/Merge.ts
@@ -55,25 +55,120 @@ export class Merge<T extends number> extends SortBase<T> {
         // 这里是对数组进行合并了
         let i: number = left, j: number = mid + 1;
         for (let k = left; k < right; k++) {
-            // 如果target[k] > target[j],则交换位置
-            if (greater(target[i], target[j], this._compareFunc)) {
-                const index = j;
-                const temp = target[k]
-                target[k] = target[j++]
-                target[index] = temp;
-                if (k === i) {
-                    i = index
-                }
-            } else {
-                if (i === k) {
-                    i++;
+            /**
+             * j > right 说明，右侧的已经排完了，这个时候只排左侧剩余的，但是由于之前排序可能导致顺序出现问题,所以重新排列剩下的数据，
+             * 此时i 为中间点，i左侧有序，i右侧有序，所以重新赋值i和j,重新进行归并
+             */
+            if (j > right) {
+                j = i;
+                i = k;
+            }
+            /**
+             * i === j 的时候有两种情况，
+             * 1、i已经遍历完成了，这个时候，k应该等于j-1,
+             * 2、i没有遍历完成，此时 k 到 i 之间的数据降序，
+             */
+            if (i === j) {
+                if (k === j) {
+                    break;
                 } else {
-                    const temp = target[k];
-                    target[k] = target[i];
-                    target[i] = temp;
+                    i = k
+                }
+            }
+            if (greater(target[i], target[j], this._compareFunc)) {
+                /**
+                 * 如果target[i] > target[j]
+                 * 1、将target[j] 复制给target[k],将原先target[k]位置上的值复制给target[j]
+                 * 2、如果此时k 和 i 相等，那么i指向原先j的位置，其他情况则不变
+                 * 3、然后j++
+                 */
+                exchange(target, k, j)
+                if (k === i) {
+                    i = j
+                }
+                j++
+            } else {
+                /**
+                 * 如果target[i] <= target[j]的时候
+                 * 1、如果i === k,那么直接i++即可
+                 * 2、如何i !== k，执行上述的逻辑，然后需要将原先的target[k] 赋值到target[i]，这个时候，如果j 和 i 之间的距离大于1，
+                 * 则需要将i向后移动一位（因为此时的target[i]会被当前的target[k]覆盖）
+                 */
+                if (k !== i) {
+                    exchange(target, k, i)
                     if (j - i > 1) {
                         i++
                     }
+                } else {
+                    i++
+                }
+            }
+        }
+        // console.log(list)
+        // console.log(left, mid, right)
+    }
+
+    /**
+     * 合并结果通过位置数组处理
+     * @param target 
+     * @param indexList 
+     * @param left 
+     * @param mid 
+     * @param right 
+     */
+    private mergeByIndex(target: Array<T>, indexList: Array<number>, left: number, mid: number, right: number) {
+        // 首先判断列表是否是有序的
+        this.isSorted(target, left, mid)
+
+        // 这里是对数组进行合并了
+        let i: number = left, j: number = mid + 1;
+        for (let k = left; k < right; k++) {
+            /**
+             * j > right 说明，右侧的已经排完了，这个时候只排左侧剩余的，但是由于之前排序可能导致顺序出现问题,所以重新排列剩下的数据，
+             * 此时i 为中间点，i左侧有序，i右侧有序，所以重新赋值i和j,重新进行归并
+             */
+            if (j > right) {
+                j = i;
+                i = k;
+            }
+            /**
+             * i === j 的时候有两种情况，
+             * 1、i已经遍历完成了，这个时候，k应该等于j-1,
+             * 2、i没有遍历完成，此时 k 到 i 之间的数据降序，
+             */
+            if (i === j) {
+                if (k === j) {
+                    break;
+                } else {
+                    i = k
+                }
+            }
+            if (greater(target[indexList[i]], target[indexList[j]], this._compareFunc)) {
+                /**
+                 * 如果target[i] > target[j]
+                 * 1、将target[j] 复制给target[k],将原先target[k]位置上的值复制给target[j]
+                 * 2、如果此时k 和 i 相等，那么i指向原先j的位置，其他情况则不变
+                 * 3、然后j++
+                 */
+                exchange(indexList, k, j)
+                if (k === i) {
+                    i = j
+                }
+                j++
+            } else {
+                /**
+                 * 如果target[i] <= target[j]的时候
+                 * 1、如果i === k,那么直接i++即可
+                 * 2、如何i !== k，执行上述的逻辑，然后需要将原先的target[k] 赋值到target[i]，这个时候，如果j 和 i 之间的距离大于1，
+                 * 则需要将i向后移动一位（因为此时的target[i]会被当前的target[k]覆盖）
+                 */
+                if (k !== i) {
+                    exchange(indexList, k, i)
+                    if (j - i > 1) {
+                        i++
+                    }
+                } else {
+                    i++
                 }
             }
         }
@@ -92,7 +187,7 @@ export class Merge<T extends number> extends SortBase<T> {
         }
         // end不存在则设置为列表长度
         if (!end && end !== 0) {
-            end = list.length
+            end = list.length - 1
         }
         // 如果开始位置大于结束位置，则直接抛出错误
         if (start > end) {
@@ -107,18 +202,14 @@ export class Merge<T extends number> extends SortBase<T> {
         return true
     }
 
-    /**
-     * 排序，利用二分，不断将数据二分归并，深度优先
-     * @param list 
-     */
-    sort(list: Array<T>, start?: number, end?: number): void {
+    sortBase(list: Array<T>, start?: number, end?: number) {
         // start如果不存在，则直接设置为0
         if (!start) {
             start = 0
         }
         // end不存在则设置为列表长度
         if (!end && end !== 0) {
-            end = list.length
+            end = list.length - 1
         }
         // start 和 end 相等的时候，说明只有一个数字，直接返回即可
         if (start === end) {
@@ -129,16 +220,67 @@ export class Merge<T extends number> extends SortBase<T> {
             throw new RangeError(`start in must less than end`)
         }
         let mid = start + ((end - start) >> 1);
-        this.sort(list, start, mid)
-        this.sort(list, mid + 1, end)
+        this.sortBase(list, start, mid)
+        this.sortBase(list, mid + 1, end)
         this.mergeOneList(list, start, mid, end)
-        console.log(list)
-        console.log(start,mid,end)
+    }
+
+    /**
+     * 排序，利用二分，不断将数据二分归并，深度优先
+     * @param list 
+     */
+    sort(list: Array<T>): void {
+        if (Array.isArray(list)) {
+            let len = list.length
+            this.sortBase(list, 0, len - 1)
+        }
+    }
+
+    sortByIndex(list: Array<T>, indexList: Array<number>, start?: number, end?: number) {
+        // start如果不存在，则直接设置为0
+        if (!start) {
+            start = 0
+        }
+        // end不存在则设置为列表长度
+        if (!end && end !== 0) {
+            end = list.length - 1
+        }
+        // start 和 end 相等的时候，说明只有一个数字，直接返回即可
+        if (start === end) {
+            return
+        }
+        // 如果开始位置大于结束位置，则直接抛出错误
+        if (start > end) {
+            throw new RangeError(`start in must less than end`)
+        }
+        let mid = start + ((end - start) >> 1);
+        this.sortByIndex(list, indexList, start, mid)
+        this.sortByIndex(list, indexList, mid + 1, end)
+        this.mergeByIndex(list, indexList, start, mid, end)
+    }
+
+    /**
+     * 位置排序
+     */
+    indexSort(list: Array<T>) {
+        let indexList = [];
+        if (Array.isArray(list)) {
+            const len = list.length;
+            for (let i = 0; i < len; i++) {
+                indexList.push(i)
+            }
+            this.sortByIndex(list, indexList, 0, len - 1)
+        }
+        return indexList
+
     }
 }
 
 const sortInterface = new Merge();
-let list = [5, 4, 3, 2, 1, 9, 10, 21, 32, 19, 7, 8, 12, 14, 15];
+// let list = [5, 4, 3, 2, 1, 9, 10, 21, 32, 19, 7, 8, 12, 14, 15];
+// let list = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let list = [5, 4, 3, 2, 1, 9, 10,];
 console.log(list);
-sortInterface.sort(list);
-console.log(list);
+const indexList = sortInterface.indexSort(list);
+// console.log(list);
+console.log(indexList);
